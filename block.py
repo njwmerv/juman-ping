f97aa6928d5372cffc65da2fd7d56fe96f0755c0
Mari Montoya <kulafumon@gmail.com>
2025-01-30 12:39:54 -0500

    Fixed None error

--- block.py
+++ block.py
@@ -10,7 +10,7 @@ class Block(Entity):
     _passthrough = {"top":False, "bot":False, "left":False, "right":False}
 
     # Magic Methods
-    def __init__(self, x : int, y : int, width : int, height : int, passthrough : dict[str, bool] = None):
+    def __init__(self, x : int, y : int, width : int, height : int, passthrough : dict[str, bool] = {}):
         super().__init__(x=x, y=y, width=width, height=height)
         for direction in self.DIRECTIONS:
             if direction in passthrough.keys() and type(passthrough[direction]) == bool:
