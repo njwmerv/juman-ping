5974039d65d5c9f679797f2dbaec12da804890d1
Mari Montoya <kulafumon@gmail.com>
2025-02-04 14:47:11 -0500

    Made CELL_SIZE 32 and lowered downwards vel to CELL_SIZE.

--- game_constants.py
+++ game_constants.py
@@ -1,13 +1,13 @@
 # Game
 FPS : int = 60
 NAME : str = 'Juman Ping'
-CELL_SIZE : int = 16
+CELL_SIZE : int = 32
 SCREEN_WIDTH : int = 1280
 SCREEN_HEIGHT : int = 800
 
 # Movement
 GRAVITY_ACC : float = 9.81
-MAX_GRAVITY_VEL: float = 61.25
+MAX_GRAVITY_VEL: float = CELL_SIZE
 
 # COLOURS
 WHITE = (255, 255, 255)
