38af1e91e680b5d7d01615d1693cc7c76ff51829
Mari Montoya <kulafumon@gmail.com>
2025-02-22 21:31:12 -0500

    Updated cell size to 16 and capped terminal velocity to half of cell_size

--- game_constants.py
+++ game_constants.py
@@ -1,13 +1,13 @@
 # Game
 FPS : int = 60
 NAME : str = 'Juman Ping'
-CELL_SIZE : int = 32
+CELL_SIZE : int = 16
 SCREEN_WIDTH : int = 1280
 SCREEN_HEIGHT : int = 800
 
 # Movement
 GRAVITY_ACC : float = 9.81
-MAX_GRAVITY_VEL: float = CELL_SIZE
+MAX_GRAVITY_VEL: float = CELL_SIZE // 2
 
 # COLOURS
 WHITE = (255, 255, 255)
