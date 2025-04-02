0b7dd067b7d893dcfa8725d139815827d97bc0e2
Mari Montoya <kulafumon@gmail.com>
2025-04-02 00:01:31 -0400

    Created Game class so save data/memory management and stuff is in Main only.

---
+++ game.py
@@ -0,0 +1,65 @@
+import pygame
+from enum import Enum
+from level import Level
+from player import Player
+from image_loader import ImageLoader
+from game_constants import FPS, NAME, SCREEN_WIDTH, SCREEN_HEIGHT
+
+# Constants
+LEVEL_DATA_BASE_PATH : str = "./Assets/levels/"
+
+# Helper
+def get_level_data(level : str) -> str:
+    return f"./Assets/levels/{level}.json"
+
+class GameState(Enum):
+    PLAY = 0
+    WIN = 1
+    QUIT = 2
+
+class Game:
+    window : pygame.Surface
+    clock : pygame.time.Clock
+    assets : ImageLoader
+    level : Level
+    player : Player
+
+    def __init__(self):
+        # PyGame Setup
+        pygame.init()
+        pygame.font.init()
+        pygame.mixer.init()
+        pygame.display.init()
+        self.window : pygame.Surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption(NAME)
+        self.clock = pygame.time.Clock()
+
+        # Load images
+        self.assets = ImageLoader()
+
+        self.level = Level(level_data=get_level_data("test"), assets=self.assets)
+
+        self.player = Player(pos=self.level.start_pos, assets=self.assets)
+
+    def run(self) -> GameState:
+        delta_time: float = self.clock.tick(FPS) / 1000.0
+
+        # Events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                return GameState.QUIT
+            self.player.handle_input(event)
+
+        self.player.move(dt=delta_time, blocks=self.level.find_near_blocks(self.player))  # Movement
+
+        # Check win?
+
+        # Drawing Everything
+        self.level.draw(self.window)
+        self.player.draw(self.window)
+
+        pygame.display.update()
+        return GameState.PLAY
+
+    def __del__(self):
+        pygame.quit()
--- main.py
+++ main.py
@@ -1,48 +1,11 @@
-import pygame
-from level import Level
-from player import Player
-from game_constants import FPS, NAME, SCREEN_WIDTH, SCREEN_HEIGHT, WHITE
-
-# PyGame Setup
-pygame.font.init()
-pygame.mixer.init()
-
-# WINDOW / SCREEN CONSTANTS
-WIN : pygame.Surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption(NAME)
-
-# FONTS
-DEBUG_FONT = pygame.font.SysFont("arial", 30)
+from game import Game, GameState
 
 if __name__ == "__main__":
-    pygame.init()
-    clock = pygame.time.Clock()
-
-    # Entities
-    LEVEL : Level = Level('./Assets/levels/test.json')
-    BLUE : Player = Player(pos=LEVEL.start_pos)
-    PLAYER : pygame.sprite.GroupSingle = pygame.sprite.GroupSingle(BLUE)
-
-    running = True
+    game : Game = Game()
+    running : bool = True
     while running:
-        delta_time : float = clock.tick(FPS) / 1000.0
-        pressed_keys = pygame.key.get_pressed()
-
-        # Events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-                running = False
-                break
-            BLUE.handle_input(event)
-
-        BLUE.move(pressed_keys, LEVEL.find_near_blocks(BLUE), delta_time) # Movement
-
-        # Drawing Everything
-        WIN.fill(WHITE)
-        LEVEL.terrain_group.draw(WIN)
-        BLUE.draw_platforms(WIN)
-        PLAYER.draw(WIN)
-
-        pygame.display.update()
-
-pygame.quit()
+        state : GameState = game.run()
+        match state:
+            case GameState.PLAY: continue
+            case GameState.QUIT: running = False
+            case GameState.WIN: continue
